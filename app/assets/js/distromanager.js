7dd41e7382fcd389c819a3800d66c205eda08b04
UnderMasterGM <115635269+UnderMasterGM@users.noreply.github.com>
2023-06-12 20:06:05 +0200

    Test0

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,7 +3,7 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'https://drive.google.com/drive/folders/1UlkzfPX4Sd9_C1SXlSxRwtSERfigkFHT?usp=sharing/distribution.json'
 exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
 
 const api = new DistributionAPI(
