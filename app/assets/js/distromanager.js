f901f014a0147f6474444ed9f5ea54b44b7e19f6
UnderMasterGM <115635269+UnderMasterGM@users.noreply.github.com>
2023-06-12 20:12:42 +0200

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,8 +3,8 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'https://drive.google.com/drive/folders/1UlkzfPX4Sd9_C1SXlSxRwtSERfigkFHT?usp=sharing/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'http://sigmacraft.fr/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://sigmacraft.fr/nebula/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
